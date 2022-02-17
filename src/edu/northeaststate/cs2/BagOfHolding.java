97c30dec18ca245556075ca6a5b064547200e586
John McMeen <jnmcmeen@northeaststate.edu>
2022-02-17 15:56:12 -0500

    finished BOH

--- src/edu/northeaststate/cs2/BagOfHolding.java
+++
@@ -1,5 +0,0 @@
-package edu.northeaststate.cs2;
-
-public class BagOfHolding {
-    //TODO - implement this
-}
---
+++ src/edu/northeaststate/cs2/examples/BagOfHolding.java
@@ -0,0 +1,9 @@
+package edu.northeaststate.cs2.examples;
+
+public class BagOfHolding {
+    private int maxWeight;
+
+    public BagOfHolding() {
+        maxWeight = 500;
+    }
+}
